e0087ea1b837b1d2dcb47ed6ed1a60e402f5081f
Alden Smith <32140838+Major-Lag98@users.noreply.github.com>
2023-03-21 21:15:52 -0700

    Fixed kristanas mistake

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonBehaviour : MonoBehaviour
 {
    int n;
    public Text myText;
    public Slider mySlider;
-   void Update() {
-      myText.text = "Current Volume: " + mySlider.value;
+   void Update() 
+   {
+        myText.text = "Current Volume: " + mySlider.value;
    }
 }
