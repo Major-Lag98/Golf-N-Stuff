7892cdbdb14b7d3fbc402bf7f08a9867a133ec4c
Alden Smith <Aldensmith06@Gmail.com>
2023-03-25 17:22:03 -0700

    Cleaned up prefabs and levels

--- Assets/Scripts/LevelSwitch.cs
+++ Assets/Scripts/LevelSwitch.cs
@@ -9,16 +9,21 @@ public class LevelSwitch : MonoBehaviour
     public string  SceneName; 
     public void SwitchLevelStr(string  SceneName)
     {
-        Debug.Log("You have clicked the button!");
-        SceneManager.LoadScene(SceneName,LoadSceneMode.Single);
-        
+        //Debug.Log("You have clicked the button!");
+        SceneManager.LoadScene(SceneName,LoadSceneMode.Single); 
 
     }
 
     public void SwitchLevelInt(int SceneIndex)
     {
-        Debug.Log("You have clicked the button!");
+        //Debug.Log("You have clicked the button!");
         SceneManager.LoadScene(SceneIndex);
+    }
 
+    public void SwitchNextLevel()
+    {
+        // load next scene by build index      
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene( currentSceneIndex + 1);
     }
 }
