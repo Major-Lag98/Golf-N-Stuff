b2810fd97dab2ee58ac13ad8016fc79302e1115b
kristiana11 <93414377+kristiana11@users.noreply.github.com>
2023-04-05 16:26:30 -0700

    Update levelSwitch script to see if the fadeout works...

--- Assets/Scripts/LevelSwitch.cs
+++ Assets/Scripts/LevelSwitch.cs
@@ -5,25 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class LevelSwitch : MonoBehaviour
 {
-    
-    public string  SceneName; 
-    public void SwitchLevelStr(string  SceneName)
-    {
-        //Debug.Log("You have clicked the button!");
-        SceneManager.LoadScene(SceneName,LoadSceneMode.Single); 
+    public string SceneName;
+    public Animator fadeAnimator; // reference to the fadeout animator component
 
+    public void SwitchLevelStr(string SceneName)
+    {
+        Debug.Log("You have clicked the button!");
+        StartCoroutine(SwitchLevelWithFade(SceneName));
     }
 
     public void SwitchLevelInt(int SceneIndex)
     {
-        //Debug.Log("You have clicked the button!");
-        SceneManager.LoadScene(SceneIndex);
+        Debug.Log("You have clicked the button!");
+        StartCoroutine(SwitchLevelWithFade(SceneIndex));
     }
 
-    public void SwitchNextLevel()
+    IEnumerator SwitchLevelWithFade(string SceneName)
     {
-        // load next scene by build index      
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene( currentSceneIndex + 1);
+        fadeAnimator.SetTrigger("FadeOut"); // play the fadeout animation
+        yield return new WaitForSeconds(1f); // wait for the animation to finish
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single); // load the new scene
+    }
+
+    IEnumerator SwitchLevelWithFade(int SceneIndex)
+    {
+        fadeAnimator.SetTrigger("FadeOut"); // play the fadeout animation
+        yield return new WaitForSeconds(1f); // wait for the animation to finish
+        SceneManager.LoadScene(SceneIndex); // load the new scene
     }
 }
