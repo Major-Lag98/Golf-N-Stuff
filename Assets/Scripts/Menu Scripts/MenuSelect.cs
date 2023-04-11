d8d219a690255bcdf10ab00d9ba2249dd5f6c057
Alden Smith <Aldensmith06@Gmail.com>
2023-04-10 21:40:27 -0700

    Overhauled main menu with animations

--- Assets/Scripts/Menu Scripts/MenuSelect.cs
+++ Assets/Scripts/Menu Scripts/MenuSelect.cs
@@ -5,65 +5,74 @@ using System.Diagnostics;
 
 public class MenuSelect : MonoBehaviour
 {
-    
+
     // the different menus
     public GameObject Main;
     public GameObject LevelSelect;
     public GameObject Options;
     public GameObject HelpMenu;
 
+    string outAnimTrigger = "out";
+    public Animator[] MainMenuOutAnimations;
+
 
     // Start is called before the first frame update
     void Start()
     {
         watch.time = new Stopwatch();
-        watch.totalScore = 0 ; 
-        SetMenuMain();
+        watch.totalScore = 0;
+
+        // start off the scene looking at the main menu
+        SetMenuHelper(LevelSelect, Options, HelpMenu, Main);
 
     }
 
     // set menu to level select
     public void SetMenuLevelSelect()
     {
-        // disable both Options, Help, and Main
-        Main.SetActive(false);
-        Options.SetActive(false);
-        HelpMenu.SetActive(false);
-        // enable Select Level
-        LevelSelect.SetActive(true);
+        StartCoroutine(ChangeMenuAndWaitForAnim(Main, Options, HelpMenu, LevelSelect));
     }
 
     // set menu to options
     public void SetMenuOptions()
     {
-        // disable both Main, help, and select
-        LevelSelect.SetActive(false);
-        Main.SetActive(false);
-        HelpMenu.SetActive(false);
-        // enable Options
-        Options.SetActive(true);
+        StartCoroutine(ChangeMenuAndWaitForAnim(LevelSelect, Main, HelpMenu, Options));
     }
 
     // set menu to main
     public void SetMenuMain()
     {
-        // disable both Options and select
-        LevelSelect.SetActive(false);
-        Options.SetActive(false);
-        HelpMenu.SetActive(false);
-        // enable Main
-        Main.SetActive(true);
+        StartCoroutine(ChangeMenuAndWaitForAnim(LevelSelect, Options, HelpMenu, Main));
     }
 
     // set menu to help menu
     public void SetMenuHelpMenu()
     {
-      // disable main, options, and select
-      LevelSelect.SetActive(false);
-      Main.SetActive(false);
-      Options.SetActive(false);
-      // enable help menu
-      HelpMenu.SetActive(true);
-   }
+        StartCoroutine(ChangeMenuAndWaitForAnim(LevelSelect, Main, Options, HelpMenu));
+    }
 
+    // disable the 3 menu objects specified and enable the one you want
+    void SetMenuHelper(GameObject firstDisable, GameObject secondDisable, GameObject thirdDisable, GameObject enable)
+    {
+        // disable the first 3
+        firstDisable.SetActive(false);
+        secondDisable.SetActive(false);
+        thirdDisable.SetActive(false);
+
+        // enable the last
+        enable.SetActive(true);
+    }
+
+    IEnumerator ChangeMenuAndWaitForAnim(GameObject firstDisable, GameObject secondDisable, GameObject thirdDisable, GameObject enable)
+    {
+        // loop through each animation and activate its "out" trigger
+        foreach (Animator animation in MainMenuOutAnimations)
+        {
+            UnityEngine.Debug.Log("Setting out trigger");
+            animation.SetTrigger(outAnimTrigger);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        SetMenuHelper(firstDisable, secondDisable, thirdDisable, enable);
+    }
 }
